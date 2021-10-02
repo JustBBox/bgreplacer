2974fa2eef4c8fd75bca6aa2f649e30720031b69
JustBBox <OsipovIvanS@yandex.ru>
2021-10-03 02:43:59 +0300

    fix req <-> res

--- index.js
+++ index.js
@@ -9,32 +9,33 @@ const express = require('express');
 const app = express();
 try {
     const port = process.env.PORT || 8080;
+    const host = '127.0.0.1';
 
-    app.post('/upload',(res,req) => {
+    app.post('/upload',(req, res) => {
         res.statusCode = 400;
         res.send({id:'post /upload || i am debugging you right now'});
     });
 
-    app.get('/list', (res,req) => {
+    app.get('/list', (req, res) => {
         res.statusCode = 401;
         res.send({id:'get /list || i am debugging you right now'});
     });
-    app.get('/image/:id', (res,req) => {
+    app.get('/image/:id', (req, res) => {
         res.statusCode = 402;
         res.send({id:'get /image/:id || i am debugging you right now'});
     });
 
-    app.delete('/image/:id', (res,req) => {
+    app.delete('/image/:id', (req, res) => {
         res.statusCode = 403;
         res.send({id:'delete image/:id || i am debugging you right now'});
     });
 
-    app.get('/merge', (res,req) => {
+    app.get('/merge', (req, res) => {
         res.statusCode = 404;
         res.send({id:'/merge || i am debugging you right now'});
     });
 
-    app.listen(port, '127.0.0.1');
+    app.listen(port,host);
 } catch (err) {
     console.log(`error: ${err.message}`)
 }
