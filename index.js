e206919f5001e966f3012d9d4063c1ab5fc22b20
JustBBox <OsipovIvanS@yandex.ru>
2021-10-03 03:27:45 +0300

    last chance for sqlite3

--- index.js
+++ index.js
@@ -2,40 +2,56 @@ const path = require('path');
 const express = require('express');
 // const api = require("./api");
 // const jpegExist = require("./middlewares/jpegExist");
-const { db } = require(path.resolve(__dirname, 'src', 'config'));
+// const { db } = require(path.resolve(__dirname, 'src', 'config'));
 // const { router } = require(path.resolve(__dirname, 'src', 'router'));
 // app.use('/', router);
 
 const app = express();
 try {
+
+    const sqlite3 = require('sqlite3');
+    let data = new sqlite3.Database(path.resolve(__dirname, 'jpegDB'), (err) => {
+        if (err) {
+            console.log('Could not open/create database', err)
+        } else {
+            // Creating the main table (id, uploadedAt, size, originalFilename)
+            data.run('CREATE TABLE IF NOT EXISTS main(id TEXT PRIMARY KEY, uploadedAt INTEGER, size INTEGER, originalFilename TEXT)', (err) => {
+                if (err) {
+                    console.log('could not create table main: ', err)
+                }
+            })
+            // console.log('Connected to sqlite table main');
+        }
+    });
+
     const port = process.env.PORT || 8080;
     const host = '127.0.0.1';
 
-    app.post('/upload',(req, res) => {
+    app.post('/upload', (req, res) => {
         res.statusCode = 400;
-        res.send({id:'post /upload || i am debugging you right now'});
+        res.send({id: 'post /upload || i am debugging you right now'});
     });
 
     app.get('/list', (req, res) => {
         res.statusCode = 401;
-        res.send({id:'get /list || i am debugging you right now'});
+        res.send({id: 'get /list || i am debugging you right now'});
     });
     app.get('/image/:id', (req, res) => {
         res.statusCode = 402;
-        res.send({id:'get /image/:id || i am debugging you right now'});
+        res.send({id: 'get /image/:id || i am debugging you right now'});
     });
 
     app.delete('/image/:id', (req, res) => {
         res.statusCode = 403;
-        res.send({id:'delete image/:id || i am debugging you right now'});
+        res.send({id: 'delete image/:id || i am debugging you right now'});
     });
 
     app.get('/merge', (req, res) => {
         res.statusCode = 404;
-        res.send({id:'/merge || i am debugging you right now'});
+        res.send({id: '/merge || i am debugging you right now'});
     });
 
-    app.listen(port,host);
+    app.listen(port, host);
 } catch (err) {
     console.log(`error: ${err.message}`)
 }
